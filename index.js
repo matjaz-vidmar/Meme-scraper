4360b3881a88e2905402a081afbc3a3ba47e5288
Matjaz Vidmar <wei.majiang@gmail.com>
2022-09-08 23:13:11 +0200

    Work in progress

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
+import cheerio from 'cheerio';
 import domtoimage from 'dom-to-image';
+import fs from 'fs';
 import fetch from 'node-fetch';
+import nodeHtmlToImage from 'node-html-to-image';
 
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
 );
-const images = await response.text();
-const imagesArray = images.split('.jpg');
-// console.log(images);
-// console.log(imagesArray);
+const body = await response.text();
+const $ = cheerio.load(body);
+let div = $('section > div');
 
-const firstTenArray = [
-  'https://memecomplete.com/edit/https://api.memegen.link/images/ch/if_you_wanted_to_avoid_the_friend_zone/you_should_have_made_your_intentions_known_from_the_start.jpg',
-  'https://api.memegen.link/images/ch/if_you_wanted_to_avoid_the_friend_zone/you_should_have_made_your_intentions_known_from_the_start.jpg?width=300',
-  'https://memecomplete.com/edit/https://api.memegen.link/images/sparta/this._is./sparta!.jpg',
-  'https://api.memegen.link/images/sparta/this._is./sparta!.jpg?width=300',
-  'https://memecomplete.com/edit/https://api.memegen.link/images/sb/remembers_the_face/but_not_the_name.jpg',
-  'https://api.memegen.link/images/sb/remembers_the_face/but_not_the_name.jpg?width=300',
-  'https://memecomplete.com/edit/https://api.memegen.link/images/disastergirl/what_did_you_say~q.jpg',
-  'https://api.memegen.link/images/disastergirl/what_did_you_say~q.jpg?width=300',
-  'https://memecomplete.com/edit/https://api.memegen.link/images/jd/disregard_females/acquire_currency.jpg',
-  'https://api.memegen.link/images/jd/disregard_females/acquire_currency.jpg?width=300',
-  'https://memecomplete.com/edit/https://api.memegen.link/images/yuno/y_u_no/use_this_meme!~q.jpg',
-];
-// console.log(firstTenArray);
-// const pics = domtoimage.toPng(firstTenArray);
-// console.log(pics);
+let sources = [];
+div.each(function () {
+  sources.push($(this).find('img').attr('src'));
+});
+
+const firstTenSources = sources.slice(0, 10);
+
+function toBuffer(firstTenSources) {
+  const buf = Buffer.alloc(firstTenSources.byteLength);
+  const view = new Uint8Array(ab);
+  for (let i = 0; i < buf.length; ++i) {
+    buf[i] = view[i];
+  }
+  return buf;
+}
+
+// let blobbb = new Blob([firstTenSources], { type: 'text/html' });
+// const blobBlob = Blob(firstTenSources);
+// const buffer = Buffer.from(blobBlob, 'base64');
+const contents = fs.readFile(firstTenSources, (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
