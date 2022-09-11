a09b6bbacc79818c183837416d4c8b0e1cc9bfbb
Matjaz Vidmar <wei.majiang@gmail.com>
2022-09-11 11:23:34 +0200

    Add stuff

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import cheerio from 'cheerio';
-import domtoimage from 'dom-to-image';
+// import { first } from 'cheerio/lib/api/traversing';
+// import domtoimage from 'dom-to-image';
 import fs from 'fs';
+// import iconvlite from 'iconv-lite';
 import fetch from 'node-fetch';
-import nodeHtmlToImage from 'node-html-to-image';
+
+// import nodeHtmlToImage from 'node-html-to-image';
 
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
@@ -16,21 +19,38 @@ div.each(function () {
   sources.push($(this).find('img').attr('src'));
 });
 
-const firstTenSources = sources.slice(0, 10);
+let firstTenArray = sources.splice(0, 10);
 
-function toBuffer(firstTenSources) {
-  const buf = Buffer.alloc(firstTenSources.byteLength);
-  const view = new Uint8Array(ab);
-  for (let i = 0; i < buf.length; ++i) {
-    buf[i] = view[i];
-  }
-  return buf;
-}
+const response2 = await fetch(firstTenArray);
+const picture = await response2.text();
 
-// let blobbb = new Blob([firstTenSources], { type: 'text/html' });
-// const blobBlob = Blob(firstTenSources);
-// const buffer = Buffer.from(blobBlob, 'base64');
-const contents = fs.readFile(firstTenSources, (err, data) => {
-  if (err) throw err;
-  console.log(data);
+// let writtenFile = fs.writeFile('./memes/01.jpg', picture);
+
+// console.log(picture);
+
+// const sourcesBuffer = Buffer.from(firstTenSources);
+
+let stream = fs.createWriteStream(picture);
+
+stream.once(picture, () => {
+  stream.write('01.jpg\n');
+  stream.write('02.jpg\n');
+  stream.write('03.jpg\n');
+  stream.write('04.jpg\n');
+  stream.write('05.jpg\n');
+  stream.write('06.jpg\n');
+  stream.write('07.jpg\n');
+  stream.write('08.jpg\n');
+  stream.write('09.jpg\n');
+  stream.write('10.jpg\n');
+
+  // Important to close the stream when you're ready
+  stream.end();
 });
+
+// console.log(stream);
+
+// const contents = fs.readFile(firstTenSources, (err, data) => {
+// if (err) throw err;
+// console.log(data);
+// });
